11d06e00e83f0e63847c2d1010d5dd0993255b96
naresh1318 <rnaresh.n@gmail.com>
2017-10-22 19:47:36 -0400

    reduced model complexity

--- play_cartpole.py
+++ play_cartpole.py
@@ -21,9 +21,9 @@ Y_target = tf.placeholder(dtype=tf.float32, shape=[None, env.action_space.n], na
 def get_agent(x, reuse=False):
     if reuse:
         tf.get_variable_scope().reuse_variables()
-    dense_1 = ops.dense(x, 8, mc.dense_1, name='dense_1')
-    dense_2 = ops.dense(dense_1, mc.dense_1, mc.dense_2, name='dense_2')
-    dense_3 = ops.dense(dense_2, mc.dense_2, mc.dense_3, name='dense_3')
+    dense_1 = tf.nn.relu(ops.dense(x, 8, mc.dense_1, name='dense_1'))
+    dense_2 = tf.nn.relu(ops.dense(dense_1, mc.dense_1, mc.dense_2, name='dense_2'))
+    dense_3 = tf.nn.relu(ops.dense(dense_2, mc.dense_2, mc.dense_3, name='dense_3'))
     output = ops.dense(dense_3, mc.dense_3, env.action_space.n, name='output')
     return output
 
@@ -34,7 +34,7 @@ def collect_observations(sess, agent, prob_rand):
     observation = np.expand_dims(observation, axis=0)
     state = np.append(observation, observation, 1)
     for i in range(mc.observation_time):
-        if np.random.rand() < prob_rand:
+        if np.random.rand() <= prob_rand:
             action = np.random.randint(low=0, high=env.action_space.n)
         else:
             action = np.argmax(sess.run(agent, feed_dict={X_input: state}))
@@ -67,12 +67,13 @@ def play(sess, agent, no_plays):
             # TODO: Comment the next line when running on the cloud
             env.render()
             action = np.argmax(sess.run(agent, feed_dict={X_input: state}))
+            print(action)
             new_state, r, done, _ = env.step(action)
             new_state = np.expand_dims(new_state, axis=0)
             state = np.expand_dims(np.append(new_state, state[0][:4]), axis=0)
             reward += r
         rewards.append(reward)
-        print("Game: {}/{}".format(p+1, no_plays))
+        print("Game: {}/{}".format(p + 1, no_plays))
         print("Reward: {}".format(reward))
     print("------------------------------------------------------------------------------------------------------")
     print("Best reward: {}".format(np.amax(rewards)))
@@ -82,7 +83,8 @@ def play(sess, agent, no_plays):
 def train(train_model=True):
     agent = get_agent(X_input)
 
-    loss = tf.reduce_mean(tf.square(agent - Y_target))
+    # loss = tf.reduce_mean(tf.square(agent - Y_target))
+    loss = tf.losses.mean_squared_error(labels=Y_target, predictions=agent)
 
     optimizer = tf.train.AdamOptimizer(learning_rate=mc.learning_rate).minimize(loss)
 
@@ -102,9 +104,10 @@ def train(train_model=True):
             prob_rand = mc.prob_random
             for e in range(mc.n_epochs):
                 print("--------------------------Epoch: {}/{}------------------------------".format(e + 1, mc.n_epochs))
-                prob_rand = prob_rand/1.1
+                if e % 11 == 0:
+                    prob_rand = prob_rand / 1.2
                 observations = collect_observations(sess, agent, prob_rand)
-                for b in range(int(len(observations)/mc.batch_size)):
+                for b in range(int(len(observations) / mc.batch_size)):
                     mini_batch = random.sample(observations, mc.batch_size)
                     agent_input = []
                     agent_target = []
@@ -122,21 +125,21 @@ def train(train_model=True):
                             agent_target.append(target[0])
                         else:
                             agent_output = sess.run(agent, feed_dict={X_input: next_state})
-                            target[0][action] = reward + mc.gamma*(np.amax(agent_output))
+                            target[0][action] = reward + mc.gamma * (np.amax(agent_output))
                             agent_target.append(target[0])
 
-                        # Training the agent for 10 iterations. Finally!!
+                            # Training the agent for 10 iterations. Finally!!
                     for i in range(10):
                         sess.run(optimizer, feed_dict={X_input: agent_input, Y_target: agent_target})
                     l, summary = sess.run([loss, summary_op], feed_dict={X_input: agent_input, Y_target: agent_target})
                     writer.add_summary(summary, global_step=step)
-                    print("Batch: {}/{}".format(b+1, int(len(observations)/mc.batch_size)))
+                    print("Batch: {}/{}".format(b + 1, int(len(observations) / mc.batch_size)))
                     print("Loss: {:.4f}".format(l))
                     step += 1
             # Save the agent
             saved_path = saver.save(sess, mc.logdir + '/model_{}'.format(datetime.datetime.now()))
             print("Time taken of {} epochs on your potato: {:.4f}s".format(mc.n_epochs, time.time() - t1))
-            print("Average time for each epoch: {:.4f}s".format((time.time() - t1)/mc.n_epochs))
+            print("Average time for each epoch: {:.4f}s".format((time.time() - t1) / mc.n_epochs))
             print("Tensorboard files saved in: {}".format(mc.logdir))
             print("Model saved in: {}".format(saved_path))
             print("Agent get to roll!")
@@ -145,6 +148,7 @@ def train(train_model=True):
             # TODO: Make the agent play the game
             saver.restore(sess, tf.train.latest_checkpoint(mc.logdir))
             play(sess, agent, mc.n_plays)
+
 
 if __name__ == '__main__':
     train(train_model=mc.train_model)
